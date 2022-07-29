b6d9ec264c4659460fa8535af47a126050794a5f
vicu <109116116+v1cu0@users.noreply.github.com>
2022-07-29 15:46:04 +0100

    No Discord RPC

---
+++ NoDiscordRPC/NoDiscordRPC/Main.cs
@@ -0,0 +1,27 @@
+﻿using ABI_RC.Helpers;
+using MelonLoader;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace NoDiscordRPC
+{
+    public class Main : MelonMod
+    {
+        public override void OnApplicationStart()
+        {
+            MelonCoroutines.Start(WaitForPresence());
+        }
+
+        public static IEnumerator WaitForPresence()
+        {
+            while (PresenceManager.instance == null) yield return null;
+
+            PresenceManager.instance.enabled = false;
+        }
+    }
+}
