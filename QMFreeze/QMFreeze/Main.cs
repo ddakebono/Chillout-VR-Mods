2c82c1b7b58d5fb772261b7a38f8b37da2bbd8b3
vicu <109116116+v1cu0@users.noreply.github.com>
2022-07-30 00:15:41 +0100

    QM Freeze

---
+++ QMFreeze/QMFreeze/Main.cs
@@ -0,0 +1,36 @@
+﻿using ABI_RC.Core.InteractionSystem;
+using ABI_RC.Systems.MovementSystem;
+using HarmonyLib;
+using MelonLoader;
+using System;
+
+namespace QMFreeze
+{
+    public class Main : MelonMod
+    {
+        public override void OnApplicationStart()
+        {
+            Type[] type = new[] { typeof(bool) }; // this stops ambigious matches, as two methods exist for 'ViewManagger.UiStateTogggle', one takea a bool for a parameter, the other doesnt.
+
+            HarmonyLib.Harmony harmonyInstance = new("vicuQM");
+            harmonyInstance.Patch(
+               typeof(CVR_MenuManager).GetMethod(nameof(CVR_MenuManager.ToggleQuickMenu), type),
+               null,
+               new HarmonyLib.HarmonyMethod(typeof(Main).GetMethod(nameof(UIToggle), AccessTools.all))
+           );
+
+            harmonyInstance.Patch(
+               typeof(ViewManager).GetMethod(nameof(ViewManager.UiStateToggle), type),
+               null,
+               new HarmonyLib.HarmonyMethod(typeof(Main).GetMethod(nameof(UIToggle), AccessTools.all))
+           );
+        }       
+
+        private static void UIToggle(bool __0)
+        {
+            MovementSystem.Instance.enabled = !__0;
+        }
+
+
+    }
+}
---
+++ QMFreeze/QMFreeze/Properties/AssemblyInfo.cs
@@ -0,0 +1,41 @@
+﻿using MelonLoader;
+using System;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("QMFreeze")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("QMFreeze")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("5b6ea6cf-c29f-407e-90c8-e1d905cd2c81")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: MelonInfo(typeof(QMFreeze.Main), "QMFreeze", "1.0.0", "vicu.", "https://github.com/v1cu0")]
+[assembly: MelonColor(ConsoleColor.Blue)]
+[assembly: MelonGame("Alpha Blend Interactive", "ChilloutVR")] 
