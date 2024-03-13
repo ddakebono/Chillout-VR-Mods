86f6dc0526f34d4cacc780041a45ced10233c86b
DDAkebono <gandvando@hotmail.com>
2024-03-12 23:26:45 -0500

    Added transpiler patch to make avatars stay "Grounded" when frozen, added MelonPref to toggle functionality

--- QMFreeze/Main.cs
+++ QMFreeze/Main.cs
@@ -1,5 +1,10 @@
-﻿using ABI_RC.Core.InteractionSystem;
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Reflection.Emit;
+using ABI_RC.Core.InteractionSystem;
 using ABI_RC.Systems.Movement;
+using ECM2;
 using MelonLoader;
 using HarmonyLib;
 
@@ -12,7 +17,16 @@ namespace QMFreeze
     
     public class Main : MelonMod
     {
+        public static bool IsQMFrozen => IsQMFreezeApplied || BetterBetterCharacterController.Instance.IsGrounded();
 
+        public static MelonPreferences_Entry<bool> EnableQMFreeze;
+        public static bool IsQMFreezeApplied;
+
+        public override void OnInitializeMelon()
+        {
+            var cat = MelonPreferences.CreateCategory("QMFreeze");
+            EnableQMFreeze = MelonPreferences.CreateEntry("QMFreeze", "EnableQMFreeze", true, "Enable QMFreeze", "Enables the QMFreeze immobilize functionality");
+        }
     }
     
     [HarmonyPatch]
@@ -23,8 +37,30 @@ namespace QMFreeze
         [HarmonyPatch(typeof(ViewManager), nameof(ViewManager.UiStateToggle), typeof(bool))]
         static void UIToggle(bool __0)
         {
+            if(!Main.EnableQMFreeze.Value && !Main.IsQMFrozen) return;
+
+            Main.IsQMFreezeApplied = __0;
+
             if (BetterBetterCharacterController.Instance != null)
                 BetterBetterCharacterController.Instance.SetImmobilized(__0);
+        }
+    }
+
+    [HarmonyPatch(typeof(BetterBetterCharacterController))]
+    class BBCCPatch
+    {
+        private static MethodInfo _groundedGetter = typeof(Character).GetMethod(nameof(Character.IsGrounded), BindingFlags.Instance | BindingFlags.Public);
+        private static MethodInfo _isFrozen = typeof(Main).GetProperty(nameof(Main.IsQMFrozen), BindingFlags.Static | BindingFlags.Public)?.GetMethod;
+
+        [HarmonyPatch("Animate")]
+        [HarmonyTranspiler]
+        private static IEnumerable<CodeInstruction> TranspileTime(IEnumerable<CodeInstruction> code)
+        {
+            return new CodeMatcher(code)
+                .MatchForward(false, new CodeMatch(OpCodes.Ldarg_0), new CodeMatch(OpCodes.Call, _groundedGetter))
+                .RemoveInstruction()
+                .SetOperandAndAdvance(_isFrozen)
+                .InstructionEnumeration();
         }
     }
 }
--- QMFreeze/Properties/AssemblyInfo.cs
+++ QMFreeze/Properties/AssemblyInfo.cs
@@ -34,9 +34,9 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.3.0")]
-[assembly: AssemblyFileVersion("1.0.3.0")]
-[assembly: MelonInfo(typeof(QMFreeze.Main), "QMFreeze", "1.0.3", "vicu. & DDAkebono", "")]
+[assembly: AssemblyVersion("1.0.4.0")]
+[assembly: AssemblyFileVersion("1.0.4.0")]
+[assembly: MelonInfo(typeof(QMFreeze.Main), "QMFreeze", "1.0.4", "vicu. & DDAkebono", "")]
 [assembly: MelonColor(255, 139,0,139)]
 [assembly: MelonAuthorColor(255, 139,0,139)]
 [assembly: MelonGame("Alpha Blend Interactive", "ChilloutVR")] 
